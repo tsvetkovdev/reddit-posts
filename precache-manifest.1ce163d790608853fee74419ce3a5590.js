2a17fc2ee43bf41546ca1bd251e762ed9ce180b1
kirill_tsvetkov <kirilltsvetkovdev@gmail.com>
2021-01-23 12:21:53 +0200

    Updates

---
+++ precache-manifest.1ce163d790608853fee74419ce3a5590.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0e17321c72fc62923939c2f9d85a291",
+    "url": "/reddit-posts/index.html"
+  },
+  {
+    "revision": "82999f4a5957d5448917",
+    "url": "/reddit-posts/static/css/main.009a7e4c.chunk.css"
+  },
+  {
+    "revision": "13d47055f7580cedf6fb",
+    "url": "/reddit-posts/static/js/2.6dcf9cb8.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/reddit-posts/static/js/2.6dcf9cb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82999f4a5957d5448917",
+    "url": "/reddit-posts/static/js/main.4c1bac0c.chunk.js"
+  },
+  {
+    "revision": "6fa6f84152752d335f0e",
+    "url": "/reddit-posts/static/js/runtime-main.d6444d04.js"
+  }
+]);
